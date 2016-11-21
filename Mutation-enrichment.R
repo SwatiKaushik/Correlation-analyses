463b33c235b6d7712e24b86fdda2132839ec7b8d
swati kaushik <swati.kaushik15@gmail.com>
2016-11-21 11:22:09 -0800

    First commit

--- Mutation-enrichment.R
+++ Mutation-enrichment.R
@@ -1,5 +1,6 @@
 # Enrichment analysis of interactors 
-# Swati Kaushik Nov 7 2016
+# Uses two data files - significantly mutated genes and a list of interactors in the A_B_C format
+# Swati Kaushik Nov 21 2016
 
 rm(list=ls())
 args <- commandArgs(TRUE)
@@ -16,15 +17,19 @@ HS.mutation.file <- levels(droplevels(mutation.fileM$V1))
 
 #interaction file
 interaction.file <- read.table(args[2], header=T)
-subset.ints <- subset(interaction.file, interaction.file$zscore>2, drop=FALSE)
-preys <- levels(droplevels(subset.ints$Prey))
+bait.unq <- unique(interaction.file$Bait)
+bait.unq.d <- levels(droplevels(bait.unq))
+bait.subset<- subset(interaction.file, interaction.file$zscore>2, drop=FALSE)
+preys <- levels(droplevels(bait.subset$Prey))
+preys.noTK <- preys[grep(paste(bait.unq.d, collapse="|"), preys, invert=TRUE)]
+#preys.noTK
 
 #cancer gene census
 cancer.census <- read.table(args[3], header =T, sep="\t", fill=TRUE, quote="")
 cancer.census.genes <- levels(droplevels(cancer.census$Gene.Symbol))
 
 #calculate overlap between args[1] and args[2]
-overlap <- intersect(preys, HS.mutation.file)
+overlap <- intersect(preys.noTK, HS.mutation.file)
 overlap.length <- length(overlap)
 preys.length <- length(preys)
 ccg.length <- length(HS.mutation.file)
@@ -43,5 +48,31 @@ write.table(sorted.subset.kinase.ints, file="sorted.subset.kinase.ints.out", sep
 #overlap with third geneset
 cancer.censusoverlap <- intersect(overlap, cancer.census.genes)
 not.covered.genes <- outersect(cancer.censusoverlap, overlap)
+
+#plot venn diagram
+
+grid.newpage()
+venn.plot <- draw.pairwise.venn( area1 = preys.length, 
+					area2 = ccg.length, 
+					cross.area = overlap.length, 
+					category =c ("SMG", "TK-interactome"),
+					lty = rep("blank",2), 
+					fill = c("light blue", "pink"), 
+					alpha = rep(0.5, 2), 
+					cat.pos = c(0, 0), 
+					cat.dist = rep(0.025, 2), 
+					scaled = TRUE,
+					lwd=4,
+					cex = 2,
+					col = rep("black",2)
+				 )
+
+#require(gridExtra)
+#grid.arrange(gTree(children=venn.plot), top= A , bottom="subtitle")
+
+#writing to file
+tiff(filename = "Venn_diagram2.tiff", compression = "lzw")
+grid.draw(venn.plot)
+dev.off()
 
 
